ae2644b73ea82d78d7b39cd16e094af1cc5d67bd
Gerhard Popp <gerhrad@poppgerhard.at>
2021-08-27 14:37:33 +0200

    new email template

--- classes/Login.php
+++ classes/Login.php
@@ -20,13 +20,12 @@ class Login extends ApiActions {
 
 		$this->GetUser();
 
-
 		wp_clear_auth_cookie();
 		wp_set_current_user($this->user->ID);
 		wp_set_auth_cookie($this->user->ID);
+		do_action('wp_login', null, $this->user);
 		wp_safe_redirect($this->redirect ? $this->redirect : get_field('field_5da3b06108b48', 'option'));
 		exit;
-
 
 	}
 
