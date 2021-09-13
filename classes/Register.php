692922d4fde196cb8952fa641bf55a5952d9c868
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-13 10:09:52 +0200

    disable linkedin registration

--- classes/Register.php
+++ classes/Register.php
@@ -17,30 +17,30 @@ class Register extends ApiActions {
 
 	public function Rergister() {
 
-
-		if ( get_user_by( 'email', $this->token->email ) ) {
-			( new Login( $this->token, $this->redirect ) );
-		} else {
-
-			$name  = $this->token->name;
-			$split = explode( ' ', $name );
-
-			$user_id = wp_insert_user( [
-				'user_pass'     => wp_generate_uuid4(),
-				'user_login'    => $name . ' ' . uniqid(),
-				'first_name'    => array_shift( $split ),
-				'last_name'     => implode( ' ', $split ),
-				'user_nicename' => $this->token->name,
-				'user_email'    => $this->token->email,
-				'role'          => 'subscriber',
-			] );
-
-			wp_clear_auth_cookie();
-			wp_set_current_user( $user_id );
-			wp_set_auth_cookie( $user_id );
-			wp_safe_redirect( $this->redirect ? $this->redirect : get_field( 'field_5da3b06108b48', 'option' ) );
-			exit;
-		}
+//no registration any more
+//		if ( get_user_by( 'email', $this->token->email ) ) {
+//			( new Login( $this->token, $this->redirect ) );
+//		} else {
+//
+//			$name  = $this->token->name;
+//			$split = explode( ' ', $name );
+//
+//			$user_id = wp_insert_user( [
+//				'user_pass'     => wp_generate_uuid4(),
+//				'user_login'    => $name . ' ' . uniqid(),
+//				'first_name'    => array_shift( $split ),
+//				'last_name'     => implode( ' ', $split ),
+//				'user_nicename' => $this->token->name,
+//				'user_email'    => $this->token->email,
+//				'role'          => 'subscriber',
+//			] );
+//
+//			wp_clear_auth_cookie();
+//			wp_set_current_user( $user_id );
+//			wp_set_auth_cookie( $user_id );
+//			wp_safe_redirect( $this->redirect ? $this->redirect : get_field( 'field_5da3b06108b48', 'option' ) );
+//			exit;
+//		}
 
 	}
 }
